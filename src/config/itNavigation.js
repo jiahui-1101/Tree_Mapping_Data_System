e8ce5a8572f00434a1577a24a015f1567b0464ba
jiahui-1101 <wongjiahui.0111@gmail.com>
2026-06-19 12:38:23 +0800

    define IT support navigation

---
+++ src/config/itNavigation.js
@@ -0,0 +1,8 @@
+export const IT_NAVIGATION = [
+  { id: "it-dashboard", label: "Dashboard" },
+  { id: "system-monitoring", label: "System Monitoring" },
+  { id: "incident-tickets", label: "Incident Tickets" },
+  { id: "user-access", label: "User Access" },
+];
+
+export const DEFAULT_IT_PAGE = IT_NAVIGATION[0].id;
