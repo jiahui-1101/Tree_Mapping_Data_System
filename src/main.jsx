0766bbef59993d6ee283aa843a08ab14a870f96e
jiahui-1101 <wongjiahui.0111@gmail.com>
2026-06-19 13:52:26 +0800

    align application entry and routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/tokens.css";
-import "./styles.css";
+import "./styles/global.css";
 import "./styles/components.css";
 import "./styles/responsive.css";
-import "./styles/it-support.css";
-import "./styles/it-support-responsive.css";
-import "./styles/ss4-operations.css";
-import "./styles/ss4-responsive.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
