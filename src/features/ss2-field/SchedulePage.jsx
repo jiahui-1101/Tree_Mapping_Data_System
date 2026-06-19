35f8e73d82e7b6911e0af32de1e2d87e8d3b66d4
jiahui-1101 <wongjiahui.0111@gmail.com>
2026-06-19 13:56:41 +0800

    align ranger planning workflows

--- src/features/ss2-field/SchedulePage.jsx
+++ src/features/ss2-field/SchedulePage.jsx
@@ -53,3 +53,4 @@ export default function SchedulePage({ tasks = [], onAddTask, showToast }) {
     </>
   );
 }
+
--- src/features/ss2-field/TaskTrackerPage.jsx
+++ src/features/ss2-field/TaskTrackerPage.jsx
@@ -59,3 +59,4 @@ export default function TaskTrackerPage({ tasks, fieldReports = [], onUpdateTask
     </>
   );
 }
+
